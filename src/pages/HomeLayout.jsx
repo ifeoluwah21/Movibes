1569f14b453d0d0d4c9b6327d57eec36dd8f7483
Ifeoulwah <spencerife.21@gmail.com>
2023-10-13 22:03:12 +0100

    movies page

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,69 +1,19 @@
 // import React from 'react'
 
+import styles from './HomeLayout.module.scss';
+
+import { Outlet } from 'react-router-dom';
 import Nav from '../components/Navigation/Nav';
+import Search from '../components/Search/Search';
 
 const HomeLayout = () => {
 	return (
-		<div>
-			{/* <nav className={styles.nav}>
-				<div className={styles.logo}>
-					<img
-						src={logo}
-						alt="Movibes"
-					/>
-				</div>
-				<ul className={styles[`nav--list`]}>
-					<li className={styles[`nav--item`]}>
-						<NavLink
-							to="/"
-							className={({ isActive }) =>
-								!isActive
-									? styles['nav--link']
-									: `${styles['nav--link']} ${styles['nav--link__active']}`
-							}>
-							Home
-						</NavLink>
-					</li>
-					<li className={styles[`nav--item`]}>
-						<NavLink
-							to={'/movies'}
-							className={({ isActive }) =>
-								!isActive
-									? styles['nav--link']
-									: `${styles['nav--link']} ${styles['nav--link__active']}`
-							}>
-							Movies
-						</NavLink>
-					</li>
-					<li className={styles[`nav--item`]}>
-						<NavLink
-							to={'/series'}
-							className={({ isActive }) =>
-								!isActive
-									? styles['nav--link']
-									: `${styles['nav--link']} ${styles['nav--link__active']}`
-							}>
-							TV series
-						</NavLink>
-					</li>
-					<li className={styles[`nav--item`]}>
-						<NavLink
-							to={'/upcoming'}
-							className={({ isActive }) =>
-								!isActive
-									? styles['nav--link']
-									: `${styles['nav--link']} ${styles['nav--link__active']}`
-							}>
-							Upcoming
-						</NavLink>
-					</li>
-				</ul>
-				<NavDesc />
-				<span className={styles[`nav--item`]}>
-					<NavLink className={styles['nav--link']}>Upcoming</NavLink>
-				</span>
-			</nav> */}
+		<div className={styles.layout}>
 			<Nav />
+			<div>
+				<Search />
+				<Outlet />
+			</div>
 		</div>
 	);
 };
