3c0846ae44e18482b684fa1b6ecef1ff75e61c11
Ifeoulwah <spencerife.21@gmail.com>
2023-10-14 14:50:28 +0100

    integrated swiper js react library

--- src/components/Movies/Trends.jsx
+++ src/components/Movies/Trends.jsx
@@ -1,6 +1,12 @@
 import Movie from './Movie';
 import styles from './Trends.module.scss';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation } from 'swiper/modules';
+import 'swiper/scss';
+import 'swiper/scss/autoplay';
+import 'swiper/scss/navigation';
+
 const Trends = () => {
 	return (
 		<section className={styles.container}>
@@ -8,12 +14,50 @@ const Trends = () => {
 				<h3>Trending</h3>
 				<span>see all</span>
 			</div>
-			<div className={styles['movie-con']}>
+			{/* <div className={styles['movie-con']}>
 				<Movie />
 				<Movie />
 				<Movie />
 				<Movie />
-			</div>
+			</div> */}
+
+			<Swiper
+				modules={[Navigation, Autoplay]}
+				spaceBetween={16}
+				slidesPerView={3}
+				navigation
+				onSlideChange={() => console.log('slide change')}
+				onSwiper={(swiper) => console.log(swiper)}>
+				<SwiperSlide
+					style={{
+						flex: '0 0 12.5rem',
+						width: '12.5rem',
+					}}>
+					<Movie />
+				</SwiperSlide>
+				<SwiperSlide
+					style={{
+						flex: '0 0 12.5rem',
+						width: '12.5rem',
+					}}>
+					<Movie />
+				</SwiperSlide>
+				<SwiperSlide
+					style={{
+						flex: '0 0 12.5rem',
+						width: '12.5rem',
+					}}>
+					<Movie />
+				</SwiperSlide>
+				<SwiperSlide
+					style={{
+						flex: '0 0 12.5rem',
+						width: '12.5rem',
+					}}>
+					<Movie />
+				</SwiperSlide>
+				...
+			</Swiper>
 		</section>
 	);
 };
